abdbe272ff448be3e97cd612b753f4632a496330
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-11-03 18:59:10 +0530

    fix: update default port from 3000 to 8080 in main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const seeder = app.get(BlogSeederService);
   await seeder.seed();
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
